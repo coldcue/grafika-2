dfa16c8aae0860125913778c70661b80708a13f2
coldcue <coldcue@gmail.com>
2014-11-04 22:09:42 +0100

    Initial Commit

---
+++ grafika-2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  grafika-2
+//
+//  Created by Andras Szell on 11/4/14.
+//  Copyright (c) 2014 Andras Szell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
